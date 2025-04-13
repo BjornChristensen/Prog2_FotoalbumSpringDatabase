58001c3ed2111c72b7424c6669f364cea9869eb9
BjornChristensen <bjorn4000@gmail.com>
2025-04-13 20:04:32 +0200

    Bjørn

--- src/test/java/org/example/fotoalbumspringdatabase/BridgeRepositoryDatabase_IntegrationTest.java
+++ src/test/java/org/example/fotoalbumspringdatabase/BridgeRepositoryDatabase_IntegrationTest.java
@@ -2,14 +2,10 @@ package org.example.fotoalbumspringdatabase;
 
 import org.example.fotoalbumspringdatabase.model.Bridge;
 import org.example.fotoalbumspringdatabase.repository.BridgeRepositoryDatabase;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 

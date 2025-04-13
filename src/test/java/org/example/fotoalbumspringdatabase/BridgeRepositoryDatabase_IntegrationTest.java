3db56d65703f393463cc2c45277f1de72d4f633c
BjornChristensen <bjorn4000@gmail.com>
2025-04-13 20:03:13 +0200

    Bjørn

---
+++ src/test/java/org/example/fotoalbumspringdatabase/BridgeRepositoryDatabase_IntegrationTest.java
@@ -0,0 +1,34 @@
+package org.example.fotoalbumspringdatabase;
+
+import org.example.fotoalbumspringdatabase.model.Bridge;
+import org.example.fotoalbumspringdatabase.repository.BridgeRepositoryDatabase;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class BridgeRepositoryDatabase_IntegrationTest {
+    @Autowired
+    BridgeRepositoryDatabase bridgeRepositoryDatabase;
+
+    @Test
+    @DisplayName("getBridgeHappyFlow()")
+    public void getBridgeHappyFlow() {
+        // Assumptions
+        String bridgeName = "Tower";
+
+        // Execution
+        Bridge bridge=bridgeRepositoryDatabase.getBridge(bridgeName);
+
+        // Validations
+        assertNotNull(bridge, "Bridge not found");
+        assertEquals(bridgeName, bridge.getName());
+    }
+}
